2ee745453bc61342c631ebb6e6c8f1afcc4bc41f
ordi2533887 <ordi2533887@educ.normandie.fr>
2026-08-10 18:25:40 +0200

    First version

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿string current_directory = Directory.GetCurrentDirectory();
+string[] parts = [];
+while (true)
+{
+    Console.Write($"{current_directory} > ");
+    string user_input = Console.ReadLine();
+    if (user_input == "list")
+    {
+        string[] files = Directory.GetFiles(current_directory);
+        foreach (string item in files)
+        {
+            Console.WriteLine(item);
+        }
+        string[] folders = Directory.GetDirectories(current_directory);
+        foreach (string folder in folders)
+        {
+            Console.WriteLine(folder);
+        }
+    }
+    else if (user_input.StartsWith("go "))
+    {
+        parts = user_input.Split(" ", 2);
+        if (parts[1] == "..")
+        {
+            var directory_parent = Directory.GetParent(current_directory);
+            current_directory = directory_parent.FullName;
+        }
+        else if (Directory.Exists(Path.Combine(current_directory, parts[1])))
+        {
+             current_directory = Path.Combine(current_directory, parts[1]);
+        }
+    }
+
+    else if (user_input == "exit")
+    {
+        break;
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# CLIexplorer
+
+Simple CLI file explorer
+
+## Usage
+
+- list : Show list of files and folders in the current directory.
+- go <target> : Change directory, .. to go to the parent directory.
