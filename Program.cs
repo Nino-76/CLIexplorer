4fee7027c5ab315656360b8803bbc7b4fadaac4c
ordi2533887 <ordi2533887@educ.normandie.fr>
2026-08-10 21:43:00 +0200

    Add file management features and other improvements

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ while (true)
 {
     Console.Write($"{current_directory} > ");
     string user_input = Console.ReadLine();
+
     if (user_input == "list")
     {
         string[] files = Directory.GetFiles(current_directory);
+        
         foreach (string item in files)
         {
             Console.WriteLine(item);
@@ -16,11 +18,17 @@ while (true)
         {
             Console.WriteLine(folder);
         }
+
+        if (files.Length == 0 && folders.Length == 0)
+        {
+            Console.WriteLine("Current directory doesn't contain any item to display.");
+        }
     }
+
     else if (user_input.StartsWith("go "))
     {
         parts = user_input.Split(" ", 2);
-        if (parts[1] == "..")
+        if (parts[1].StartsWith(".."))
         {
             var directory_parent = Directory.GetParent(current_directory);
             current_directory = directory_parent.FullName;
@@ -29,6 +37,58 @@ while (true)
         {
              current_directory = Path.Combine(current_directory, parts[1]);
         }
+    }
+
+    else if (user_input.StartsWith("delete "))
+    {
+        parts = user_input.Split(" ", 2);
+        string path = Path.Combine(current_directory, parts[1]);
+        if (File.Exists(path))
+        {
+            Console.Write("Are you sure ? (y/n) > ");
+            user_input = Console.ReadLine();
+            if (user_input == "y")
+            {
+                File.Delete(path);   
+            }   
+        }
+        else if (Directory.Exists(path))
+        {
+            Console.Write("Are you sure ? (y/n) > ");
+            user_input = Console.ReadLine();
+            if (user_input == "y")
+            {
+                Directory.Delete(path);
+            }
+        }
+
+    }
+
+    else if (user_input.StartsWith("makefile "))
+    {
+        parts = user_input.Split(" ", 2);
+        string path = Path.Combine(current_directory, parts[1]);
+        File.Create(path).Dispose();
+    }
+
+    else if (user_input.StartsWith("makedir "))
+    {
+        parts = user_input.Split(" ", 2);
+        string path = Path.Combine(current_directory, parts[1]);
+        Directory.CreateDirectory(path);
+    }
+
+    else if (user_input == "help")
+    {
+        Console.WriteLine("""
+        - list : Show the list of files and folders in the current directory
+        - go : Change directory, use .. to go to the parent directory
+        - delete : Delete a chosen file or directory (only works on empty directories)
+        - makefile : Make a new empty file
+        - makedir : Make a new empty directory
+        - exit : Exit the app
+        - help : Show this message
+        """);
     }
 
     else if (user_input == "exit")
--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # CLIexplorer
 
-Simple CLI file explorer
+A simple command-line file explorer.
 
 ## Usage
 
-- list : Show list of files and folders in the current directory.
-- go <target> : Change directory, .. to go to the parent directory.
+- list : Show the list of files and folders in the current directory
+- go : Change directory, use .. to go to the parent directory
+- delete : Delete a chosen file or directory (only works on empty directories)
+- makefile : Make a new empty file
+- makedir : Create a new empty directory
+- exit : Exit the app
+- help : Show every command
